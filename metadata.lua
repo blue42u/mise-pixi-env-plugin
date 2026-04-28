e42ac4cbe0e93ffd90ec7d27c84f06c29b1f8d52
Jonathon Anderson <anderson.jonathonm@gmail.com>
2026-04-28 08:20:17 -0500

    Update metadata.lua
    Signed-off-by: Jonathon Anderson <anderson.jonathonm@gmail.com>

--- metadata.lua
+++ metadata.lua
@@ -2,7 +2,7 @@
 --- Documentation: https://mise.jdx.dev/env-plugin-development.html
 PLUGIN = {
     --- Plugin name (required)
-    name = "pixi",
+    name = "pixi-env",
 
     --- Plugin version (required)
     version = "0.0.1",
